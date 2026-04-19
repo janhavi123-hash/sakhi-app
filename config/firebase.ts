cdfba78a2b5042f51641ed1eb0900d1a2eadeb05
Janhavi <janhavisonawane171@gmail.com>
2026-04-19 22:28:46 +0530

    firebase fix

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: "1:845725300818:web:09beef3c0011c9d050078b"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
